ce1e3b84bf648fb64282f87887f264555fe9dcd5
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-17 09:47:16 +0900

    add: sidebar contents
    ・サイドバーに遷移する予定のページの一覧を追加しました。

--- src/resources/ts/components/organisms/Sidebar.tsx
+++ src/resources/ts/components/organisms/Sidebar.tsx
@@ -1,3 +1,24 @@
+import { MenuItem, MenuList, Stack } from "@mui/material";
+import { AfterLoginRouter } from "../../router/AfterLoginRouter";
+import { BeforeLoginRouter } from "../../router/BeforeLoginRouter";
+
 export const Sidebar = () => {
-    return <></>;
+    const authentication = true;
+    return (
+        <Stack direction="row" mt="10px">
+            <MenuList sx={{ width: "100%" }}>
+                {authentication
+                    ? AfterLoginRouter.map((router) => (
+                          <MenuItem key={router.path} sx={{ height: "100px" }}>
+                              {router.name}
+                          </MenuItem>
+                      ))
+                    : BeforeLoginRouter.map((router) => (
+                          <MenuItem key={router.path} sx={{ height: "100px" }}>
+                              {router.name}
+                          </MenuItem>
+                      ))}
+            </MenuList>
+        </Stack>
+    );
 };
