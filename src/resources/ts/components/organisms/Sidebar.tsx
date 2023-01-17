9e6f3ef73de0cbd37fa8d2c4797839b4a1270b84
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-17 10:04:17 +0900

    add: onClick of sidebar contents
    ・サイドバーのメニューをクリックした時にそのページに遷移する機能を追加しました。

--- src/resources/ts/components/organisms/Sidebar.tsx
+++ src/resources/ts/components/organisms/Sidebar.tsx
@@ -1,20 +1,33 @@
 import { MenuItem, MenuList, Stack } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 import { AfterLoginRouter } from "../../router/AfterLoginRouter";
 import { BeforeLoginRouter } from "../../router/BeforeLoginRouter";
 
 export const Sidebar = () => {
+    const navigate = useNavigate();
     const authentication = true;
+    const onClickMenu = (path: string) => {
+        navigate(path);
+    };
     return (
         <Stack direction="row" mt="10px">
             <MenuList sx={{ width: "100%" }}>
                 {authentication
                     ? AfterLoginRouter.map((router) => (
-                          <MenuItem key={router.path} sx={{ height: "100px" }}>
+                          <MenuItem
+                              key={router.path}
+                              sx={{ height: "100px" }}
+                              onClick={() => onClickMenu(router.path)}
+                          >
                               {router.name}
                           </MenuItem>
                       ))
                     : BeforeLoginRouter.map((router) => (
-                          <MenuItem key={router.path} sx={{ height: "100px" }}>
+                          <MenuItem
+                              key={router.path}
+                              sx={{ height: "100px" }}
+                              onClick={() => onClickMenu(router.path)}
+                          >
                               {router.name}
                           </MenuItem>
                       ))}
--- src/resources/ts/router/Router.tsx
+++ src/resources/ts/router/Router.tsx
@@ -1,35 +1,31 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Page404 } from "../components/pages/Page404";
 import { AfterLoginRouter } from "./AfterLoginRouter";
 import { BeforeLoginRouter } from "./BeforeLoginRouter";
 
 export const Router = () => {
     const authentication = true;
-    return (
-        <BrowserRouter>
-            {authentication ? (
-                <Routes>
-                    {AfterLoginRouter.map((route) => (
-                        <Route
-                            key={route.path}
-                            path={route.path}
-                            element={route.children}
-                        />
-                    ))}
-                    <Route path="*" element={<Page404 />} />
-                </Routes>
-            ) : (
-                <Routes>
-                    {BeforeLoginRouter.map((route) => (
-                        <Route
-                            key={route.path}
-                            path={route.path}
-                            element={route.children}
-                        />
-                    ))}
-                    <Route path="*" element={<Page404 />} />
-                </Routes>
-            )}
-        </BrowserRouter>
+    return authentication ? (
+        <Routes>
+            {AfterLoginRouter.map((route) => (
+                <Route
+                    key={route.path}
+                    path={route.path}
+                    element={route.children}
+                />
+            ))}
+            <Route path="*" element={<Page404 />} />
+        </Routes>
+    ) : (
+        <Routes>
+            {BeforeLoginRouter.map((route) => (
+                <Route
+                    key={route.path}
+                    path={route.path}
+                    element={route.children}
+                />
+            ))}
+            <Route path="*" element={<Page404 />} />
+        </Routes>
     );
 };
