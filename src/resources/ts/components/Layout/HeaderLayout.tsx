86f6d74a35f4f2d4324198a59fd4246e78167873
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-18 10:40:31 +0900

    add: show sub title on header
    ・ヘッダーに表示しているページの名前を表示するようにしました。

--- src/resources/ts/components/Layout/HeaderLayout.tsx
+++ src/resources/ts/components/Layout/HeaderLayout.tsx
@@ -1,17 +1,43 @@
 import { AppBar, Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import { MainRouters } from "../../router/MainRouters";
 
 export const HeaderLayout = () => {
+    const authenticate = true;
+    const { pathname } = useLocation();
+    const [subTitle, setSubTitle] = useState("");
+    useEffect(() => {
+        const castedPathname = pathname.slice(1);
+        const arrayMainRouters = Object.entries(MainRouters);
+        const showRoute = arrayMainRouters.find(([key, value]) => {
+            return key == castedPathname;
+        });
+        if (showRoute && showRoute[1].authenticate === authenticate)
+            setSubTitle(showRoute[1].name);
+        else setSubTitle("Page Not Found");
+    }, [pathname]);
+
     return (
         <AppBar position="absolute">
-            <Box height="60px">
+            <Box height="60px" display="flex">
                 <Typography
                     component="h1"
                     variant="h5"
                     pt="20px"
                     pb="10px"
-                    pl="20px"
+                    pl="80px"
                 >
                     ProjectA
+                </Typography>
+                <Typography
+                    component="h3"
+                    variant="h6"
+                    pt="20px"
+                    pb="10px"
+                    mx="auto"
+                >
+                    {subTitle}
                 </Typography>
             </Box>
         </AppBar>
